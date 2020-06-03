f6fca9545d27d5f14b907823540c2f858017e8f3
Mykola Rudnev <RudnevMykola@gmail.com>
2020-06-03 21:27:43 +0200

    added cloning bacone img

--- app/scripts/cloneBacone.js
+++ app/scripts/cloneBacone.js
@@ -1,6 +1,6 @@
 function CloneBacone() {
-    const bacone =  document.getElementById("bacon");
-    const cloneBacone = bacone.cloneNode(true);
-    const layout = document.getElementById("overview")
-    layout.appendChild(cloneBacone);
-  }
+  const bacone =  document.getElementById("bacon");
+  const cloneBacone = bacone.cloneNode(true);
+  const layout = document.getElementById("overview")
+  layout.appendChild(cloneBacone);
+} 

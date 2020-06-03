7e8539251531c0cb456d3944743c86fa7c7b2ac2
Mykola Rudnev <RudnevMykola@gmail.com>
2020-06-03 21:04:15 +0200

    Added cloning bacon img

---
+++ app/scripts/cloneBacone.js
@@ -0,0 +1,6 @@
+function CloneBacone() {
+    const bacone =  document.getElementById("bacon");
+    const cloneBacone = bacone.cloneNode(true);
+    const layout = document.getElementById("overview")
+    layout.appendChild(cloneBacone);
+  }

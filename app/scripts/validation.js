3c5db1e9ebe0a58347d151500a8b26942747a1a5
Mykola Rudnev <RudnevMykola@gmail.com>
2020-06-07 18:08:28 +0200

    Added checkout to tabs and more validation on button validation + some style correction

--- app/scripts/validation.js
+++ app/scripts/validation.js
@@ -2,11 +2,17 @@ function validate() {
   document.querySelectorAll('.validate-input').forEach(node => {
     if (node.value == "" || node.value === undefined) {
       node.classList.add("error")
-
-      return false;
     } else {
       node.classList.remove("error")
+      node.classList.add("success")
     }
-    return true
   })
+  
+ const validateField = document.querySelectorAll('.success');
+    if (validateField.length=== 9){
+      alert("Purchase completed wait for an email")
+    }else{
+      alert("Fulfill all fields ")
+    }
+ 
 }
